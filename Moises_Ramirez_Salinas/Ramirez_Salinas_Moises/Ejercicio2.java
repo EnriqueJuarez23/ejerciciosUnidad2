0df3e0c6cf7662c95ad02443e6fc7ab1bf1a4dc1
MoiRamS <163018512+moimods@users.noreply.github.com>
2024-10-07 22:57:49 -0600

    Add files via upload

---
+++ Moises_Ramirez_Salinas/Ramirez_Salinas_Moises/Ejercicio2.java
@@ -0,0 +1,7 @@
+ class Ejercicio2 { //clase
+    public static void main(String[]args){ //metodo main
+        String nombre = "Moises Ramirez Salinas"; //declaro variable
+        System.out.println("Bienvenido:" + nombre); //impresion de mi nombre con concatenacion
+    }
+    
+}
